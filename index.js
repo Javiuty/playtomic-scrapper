5bbfeebbd89f148d4bbad29fdbec9a0bbc0863df
Javiuty <javiiescuadra@gmail.com>
2025-03-06 18:09:11 +0100

    feat: add eslint + standard

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-import { CLUBS } from "./src/constants.js";
-import { getDates } from "./src/utils.js";
-import { scrape } from "./src/scraper.js";
-import { html } from './src/templates.js';
+import { CLUBS } from './src/constants.js'
+import { getDates } from './src/utils.js'
+import { scrape } from './src/scraper.js'
 
-import { sendEmail } from "./src/mail.js";
+import { sendEmail } from './src/mail.js'
 
 const slots = []
 const dates = getDates()
 const urls = CLUBS.map(club => dates.map(date => club.replace('0000-00-00', date))).flat()
 
-
 // await sendEmail()
-async function main() {
+async function main () {
   for (const url of urls) {
     slots.push(await scrape(url))
   }
